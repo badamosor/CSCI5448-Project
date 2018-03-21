0dad576b0f573f28514c8fcfe63afd5ba1fb9ee8
Badamosor Khaltar <badamosor@gmail.com>
2018-03-20 22:51:12 -0600

    Implemented addSongs and getSongs

--- Code/Playlist.py
+++ Code/Playlist.py
@@ -2,12 +2,21 @@ import DBQuery
 import Song
 
 class Playlist ():
+
     songs = []
 
     def addSong(self, id):
         self.songs.append(id)
 
+    def addSongs(self, ids):
+        self.songs.extend(ids)
+
+    def getSongs(self):
+        return self.songs
+
 if __name__ == '__main__':
     myPlaylist = Playlist()
     myPlaylist.addSong(2)
+    myPlaylist.addSongs([1, 3])
+    print myPlaylist.getSongs()
 
