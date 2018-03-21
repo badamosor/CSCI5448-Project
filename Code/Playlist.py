8fc3fcc6841482c508a0a9e326e4e7fadde35aab
Badamosor Khaltar <badamosor@gmail.com>
2018-03-20 22:55:56 -0600

    Implemented deleteSong

--- Code/Playlist.py
+++ Code/Playlist.py
@@ -11,12 +11,18 @@ class Playlist ():
     def addSongs(self, ids):
         self.songs.extend(ids)
 
+    def deleteSong(self, id):
+        self.songs.remove(id)
+
     def getSongs(self):
         return self.songs
+    
 
 if __name__ == '__main__':
     myPlaylist = Playlist()
     myPlaylist.addSong(2)
     myPlaylist.addSongs([1, 3])
+    print myPlaylist.getSongs()
+    myPlaylist.deleteSong(1)
     print myPlaylist.getSongs()
 
