de9c3108189a74f4130bfe68ac55467bd46f9da0
LukeMeszar <lume0018@colorado.edu>
2018-04-04 23:30:20 -0600

    Attempt at getting login to work

--- Code/playlistmanager/playlist_manager/urls.py
+++ Code/playlistmanager/playlist_manager/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_user/', views.create_user, name='create_user'),
+    path('login/', views.login, name='login'),
+    path('artist_list', views.artist_list, name='artist_list'),
     path('<int:artist_id>/', views.artist_detail, name='artist_detail'),
     path('<int:album_id>/album', views.album_detail, name='album_detail'),
     path('<int:song_id>/song', views.song_detail, name='song_detail'),
--- Code/playlistmanager/playlist_manager/views.py
+++ Code/playlistmanager/playlist_manager/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 from .forms import UserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
@@ -8,8 +8,7 @@ from .models import Artist, Album, Song
 
 # Create your views here.
 def index(request):
-    artist_list = Artist.objects.order_by('artist_name')[:5]
-    context = {'artist_list': artist_list,}
+    context = {}
     return render(request, 'playlist_manager/index.html', context)
 
 def create_user(request):
@@ -18,11 +17,42 @@ def create_user(request):
         if form.is_valid():
             new_user = User.objects.create_user(**form.cleaned_data)
             login(request, new_user)
-            return HttpResponseRedirect('/playlistmanager/')
+            return HttpResponseRedirect('/playlistmanager/artist_list')
     else:
         form = UserForm()
 
     return render(request, 'playlist_manager/create_user.html', {'form': form})
+
+def login(request):
+    # username = request.POST['username']
+    # password = request.POST['password']
+    # user = authenticate(request, username=username, password=password)
+    # if user is not None:
+    #     login(request,user)
+    #     return HttpResponseRedirect('/playlistmanager/artist_list')
+    #
+    # return render(request, 'playlist_manager/login.html', {})
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.username
+            password =  form.password
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, new_user)
+                return HttpResponseRedirect('/playlistmanager/artist_list')
+            else:
+                return HttpResponseRedirect('/playlistmanager/')
+
+    else:
+        form = UserForm()
+
+    return render(request, 'playlist_manager/login.html', {'form': form})
+
+def artist_list(request):
+    artist_list = Artist.objects.order_by('artist_name')[:5]
+    context = {'artist_list': artist_list,}
+    return render(request, 'playlist_manager/artist_list.html', context)
 
 def artist_detail(request, artist_id):
     artist = get_object_or_404(Artist, pk=artist_id)
