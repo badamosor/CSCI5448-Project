4445fc506dfe2e831cea2c8aca965fe9928e4413
Badamosor Khaltar <badamosor@gmail.com>
2018-04-09 14:05:36 -0600

    Added Playlist model
    Conflicts:
    	Code/playlistmanager/db.sqlite3

--- Code/playlistmanager/playlist_manager/admin.py
+++ Code/playlistmanager/playlist_manager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Song, Album, Artist
+from .models import Song, Album, Artist, Playlist
 # Register your models here.
 admin.site.register(Song)
 admin.site.register(Album)
 admin.site.register(Artist)
+admin.site.register(Playlist)
--- Code/playlistmanager/playlist_manager/models.py
+++ Code/playlistmanager/playlist_manager/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 
@@ -23,3 +25,13 @@ class Artist(models.Model):
     songs = models.ManyToManyField(Song)
     def __str__(self):
         return self.artist_name
+
+class Playlist(models.Model):
+    playlist_name = models.CharField(max_length = 200, help_text="Enter a playlist name")
+    playlist_description = models.CharField(max_length = 1000, help_text="Enter a discription for your playlist", default = '')
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null = True, related_name = 'playlist')
+    collaborative_status = models.BooleanField(default = False)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.playlist_name
