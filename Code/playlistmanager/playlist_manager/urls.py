d55a0e0ca0ac96dcbb3684490646f10419b53833
LukeMeszar <lume0018@colorado.edu>
2018-04-10 23:37:59 -0600

    Changed all views to class based

--- Code/playlistmanager/playlist_manager/urls.py
+++ Code/playlistmanager/playlist_manager/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
-
+from .views import Index, ArtistList, ArtistDetail, AlbumDetail,SongDetail,\
+Welcome, PlaylistDetail, PlaylistList, CreatePlaylist, CreateUser
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('welcome', views.welcome, name='welcome'),
-    path('create_user/', views.create_user, name='create_user'),
-    path('create_playlist/', views.create_playlist, name='create_playlist'),
-    path('artist_list', views.artist_list, name='artist_list'),
+    path('', Index.as_view(), name='index'),
+    path('welcome', login_required(Welcome.as_view()), name='welcome'),
+    path('create_user/', CreateUser.as_view(), name='create_user'),
+    path('create_playlist/', login_required(CreatePlaylist.as_view()), name='create_playlist'),
+    path('artist_list', login_required(ArtistList.as_view())),
     path('<int:song_id>/add_song_to_playlist', views.add_song_to_playlist, name='add_song_to_playlist'),
-    path('playlist_list', views.playlist_list, name='playlist_list'),
-    path('<int:artist_id>/', views.artist_detail, name='artist_detail'),
-    path('<int:album_id>/album', views.album_detail, name='album_detail'),
-    path('<int:song_id>/song', views.song_detail, name='song_detail'),
-    path('<int:playlist_id>/playlist', views.playlist_detail, name='playlist_detail'),
+    path('playlist_list', login_required(PlaylistList.as_view()), name='playlist_list'),
+    path('<int:artist_id>/', login_required(ArtistDetail.as_view()), name='artist_detail'),
+    path('<int:album_id>/album', login_required(AlbumDetail.as_view()), name='album_detail'),
+    path('<int:song_id>/song', login_required(SongDetail.as_view()), name='song_detail'),
+    path('<int:playlist_id>/playlist', login_required(PlaylistDetail.as_view()), name='playlist_detail'),
 ]
--- Code/playlistmanager/playlist_manager/views.py
+++ Code/playlistmanager/playlist_manager/views.py
@@ -1,74 +1,77 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 from .forms import UserForm, PlaylistForm
 from django.contrib.auth import login, authenticate
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
+from django.views import View
+from django.urls import reverse
 from .models import Artist, Album, Song, Playlist
 
 import logging
 logger = logging.getLogger(__name__)
 
-# Create your views here.
-def index(request):
-    context = {}
-    return render(request, 'playlist_manager/index.html', context)
+class Index(View):
+    def get(self,request):
+        context = {}
+        return render(request, 'playlist_manager/index.html', context)
 
-def create_user(request):
-    if request.method == "POST":
+class CreateUser(View):
+    def get(self, request):
+        form = UserForm()
+        return render(request, 'playlist_manager/create_user.html', {'form': form})
+    def post(self, request):
         form = UserForm(request.POST)
         if form.is_valid():
             new_user = User.objects.create_user(**form.cleaned_data)
             login(request, new_user)
-            return HttpResponseRedirect('/playlistmanager/playlist_list')
-    else:
-        form = UserForm()
+            return HttpResponseRedirect('/playlistmanager/welcome')
 
-    return render(request, 'playlist_manager/create_user.html', {'form': form})
+class ArtistList(View):
+    def get(self,request):
+        artist_list = Artist.objects.order_by('artist_name')[:5]
+        context = {'artist_list': artist_list,}
+        return render(request, 'playlist_manager/artist_list.html', context)
 
-@login_required(login_url='/accounts/login/')
-def artist_list(request):
-    artist_list = Artist.objects.order_by('artist_name')[:5]
-    context = {'artist_list': artist_list,}
-    return render(request, 'playlist_manager/artist_list.html', context)
-@login_required(login_url='/accounts/login/')
+class ArtistDetail(View):
+    def get(self,request, artist_id):
+        artist = get_object_or_404(Artist, pk=artist_id)
+        albums = get_list_or_404(artist.albums.order_by('album_name'))
+        songs = get_list_or_404(artist.songs.order_by('song_name'))
+        return render(request, 'playlist_manager/artist_detail.html', {'artist': artist, 'albums': albums, 'songs': songs})
 
-def artist_detail(request, artist_id):
-    artist = get_object_or_404(Artist, pk=artist_id)
-    albums = get_list_or_404(artist.albums.order_by('album_name'))
-    songs = get_list_or_404(artist.songs.order_by('song_name'))
-    return render(request, 'playlist_manager/artist_detail.html', {'artist': artist, 'albums': albums, 'songs': songs})
-@login_required(login_url='/accounts/login/')
-
-def album_detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    songs = get_list_or_404(album.songs.order_by('song_name'))
-    return render(request, 'playlist_manager/album_detail.html', {'album': album, 'songs': songs})
-
-@login_required(login_url='/accounts/login/')
-def song_detail(request, song_id):
-    song = get_object_or_404(Song, pk=song_id)
-    return render(request, 'playlist_manager/song_detail.html', {'song': song})
+class AlbumDetail(View):
+    def get(self, request, album_id):
+        album = get_object_or_404(Album, pk=album_id)
+        songs = get_list_or_404(album.songs.order_by('song_name'))
+        return render(request, 'playlist_manager/album_detail.html', {'album': album, 'songs': songs})
 
 
-@login_required(login_url='/accounts/login/')
-def playlist_list(request):
-    current_user = request.user
-    playlist_list = Playlist.objects.filter(owner=current_user).order_by('playlist_name')
-    context = {'playlist_list': playlist_list}
-    return render(request, 'playlist_manager/playlist_list.html', context)
+class SongDetail(View):
+    def get(self, request, song_id):
+        song = get_object_or_404(Song, pk=song_id)
+        return render(request, 'playlist_manager/song_detail.html', {'song': song})
 
-@login_required(login_url='/accounts/login/')
-def playlist_detail(request, playlist_id):
-    playlist = get_object_or_404(Playlist, pk=playlist_id)
-    songs = playlist.songs.order_by('song_name')
-    logging.warning(type(songs))
-    return render(request, 'playlist_manager/playlist_detail.html', {'playlist': playlist, 'songs': songs})
+class PlaylistList(View):
+    def get(self, request):
+        current_user = request.user
+        playlist_list = Playlist.objects.filter(owner=current_user).order_by('playlist_name')
+        context = {'playlist_list': playlist_list}
+        return render(request, 'playlist_manager/playlist_list.html', context)
 
-@login_required(login_url='/accounts/login/')
-def create_playlist(request):
-    if request.method == "POST":
+class PlaylistDetail(View):
+    def get(self, request, playlist_id):
+        playlist = get_object_or_404(Playlist, pk=playlist_id)
+        songs = playlist.songs.order_by('song_name')
+        logging.warning(type(songs))
+        return render(request, 'playlist_manager/playlist_detail.html', {'playlist': playlist, 'songs': songs})
+
+
+class CreatePlaylist(View):
+    def get(self, request):
+        form = PlaylistForm()
+        return render(request, 'playlist_manager/create_playlist.html', {'form': form})
+    def post(self, request):
         form = PlaylistForm(request.POST)
         current_user = request.user
         if form.is_valid():
@@ -78,17 +81,12 @@ def create_playlist(request):
             new_playlist = Playlist(playlist_name=playlist_name, playlist_description=playlist_description, owner=current_user, collaborative_status = collaborative_status)
             new_playlist.save()
             return HttpResponseRedirect('/playlistmanager/playlist_list')
-    else:
-        form = PlaylistForm()
-
-    return render(request, 'playlist_manager/create_playlist.html', {'form': form})
 
 @login_required(login_url='/accounts/login/')
 def add_song_to_playlist(request, song_id):
     song = get_object_or_404(Song, pk=song_id)
     current_user = request.user
     playlists = Playlist.objects.filter(owner=current_user).order_by('playlist_name')
-    logging.warning(type(playlists))
     if request.method == "POST":
         try:
             selected_playlist = playlists.get(pk=request.POST['playlist'])
@@ -100,10 +98,32 @@ def add_song_to_playlist(request, song_id):
 
     return render(request, 'playlist_manager/add_song_to_playlist.html', {'playlists': playlists})
 
-@login_required(login_url='/accounts/login/')
-def welcome(request):
-    current_user = request.user
-    artist_list = Artist.objects.order_by('artist_name')[:5]
-    playlist_list = Playlist.objects.filter(owner=current_user).order_by('playlist_name')
-    context = {'artist_list': artist_list,'playlist_list': playlist_list,}
-    return render(request, 'playlist_manager/welcome.html', context)
+class PlaylistEdit(View):
+    def get(self, request, song_id):
+        song = get_object_or_404(Song, pk=song_id)
+        current_user = request.user
+        playlists = Playlist.objects.filter(owner=current_user).order_by('playlist_name')
+        return render(request, 'playlist_manager/add_song_to_playlist.html', {'playlists': playlists})
+    def post(self, request, song_id):
+        song = get_object_or_404(Song, pk=song_id)
+        current_user = request.user
+        playlists = Playlist.objects.filter(owner=current_user).order_by('playlist_name')
+        try:
+            selected_playlist = playlists.get(pk=request.POST['playlist'])
+        except (KeyError, Playlist.DoesNotExist):
+            return render(request, 'playlist_manager/'+song_id +'/add_song_to_playlist', {'playlists': playlists,\
+            'error_message': "You didn't select a playlist"})
+        else:
+            selected_playlist.songs.add(song)
+            return render(request, 'playlist_manager/'+song_id +'/add_song_to_playlist', {'playlists': playlists,\
+            'success_message': 'Added '+song.name+'to playlist '+selected_playlist.playlist_name})
+
+
+
+class Welcome(View):
+    def get(self, request):
+        current_user = request.user
+        artist_list = Artist.objects.order_by('artist_name')[:5]
+        playlist_list = Playlist.objects.filter(owner=current_user).order_by('playlist_name')
+        context = {'artist_list': artist_list,'playlist_list': playlist_list,}
+        return render(request, 'playlist_manager/welcome.html', context)
