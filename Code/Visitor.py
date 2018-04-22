19a1b7eaf45a9a15d366a5b7026fcb845a616e44
Paul McCumber <pmccumber@gmail.com>
2018-04-21 19:51:13 -0600

    Modified the content added and added a few more albums

--- Code/Visitor.py
+++ Code/Visitor.py
@@ -63,24 +63,33 @@ class MusicVisitorImplementation(MusicVisitor):
 
     def __init__(self, pattern):
         self.pattern = pattern
+        self.results ={}
 
     def visit(self, item):
         if isinstance(item, Song):
             print ("These are the Song matches: ")
+            self.results['song']  = item.find(self.pattern)
         elif isinstance(item, Artist):
             print ("These are the Artist matches: ")
+            self.results['artist']  = item.find(self.pattern)
         elif isinstance(item, Album):
             print ("These are the Album matches: ")
+            self.results['album']  = item.find(self.pattern)
+
         for match in item.find(self.pattern):
             print ("    " + item.getName(match[0]))
+
+        return self.results
 
 
 class SearchVisitor():
 
+    
     def searchDatabase (self, items, term):
         visitor = MusicVisitorImplementation(term)
         for item in items:
-            item.accept(visitor)
+            results = item.accept(visitor)
+        return results
 
 if __name__ == '__main__':
     print ("\nSearch Term: the")
@@ -91,3 +100,7 @@ if __name__ == '__main__':
     total = sv.searchDatabase(items, "the")
     print ("\n\nSearch Term: Blue")
     total = sv.searchDatabase(items, "Blue")
+
+    print (total)
+
+   
--- Code/addAlbum.py
+++ Code/addAlbum.py
@@ -2,7 +2,7 @@ import sqlite3
 import sys
 
 ### Create the connection to the database
-conn = sqlite3.connect('music.db')
+conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
 ### Open the file and read it 
@@ -10,30 +10,30 @@ fh = open(sys.argv[1],"r")
 artist = fh.readline()
 album  = fh.readline()
 
-sql = "SELECT id FROM Artist WHERE name=?"
+sql = "SELECT id FROM playlist_manager_artist WHERE artist_name=?"
 c.execute(sql, (artist.rstrip(),))
 artistId = c.fetchone()
 if artistId == None:
    ### Store the Artist
-   c.execute("INSERT INTO Artist (name) values (?)", (artist.rstrip(),))
+   c.execute("INSERT INTO playlist_manager_artist (artist_name) values (?)", (artist.rstrip(),))
    artistId = c.lastrowid
 else:
    artistId = artistId[0]
 print (artistId)
 
 ### Store the Album
-c.execute("INSERT INTO Album (name) values (?)", (album.rstrip(),))
+c.execute("INSERT INTO playlist_manager_album (album_name) values (?)", (album.rstrip(),))
 albumId = c.lastrowid
 
 ### Store the Artist/Album Association
-c.execute("INSERT INTO Artist2Album VALUES (?, ?)", (artistId, albumId))
+c.execute("INSERT INTO playlist_manager_artist_albums (artist_id, album_id) VALUES (?, ?)", (artistId, albumId))
 
 song = fh.readline()
 while song:
-    c.execute("INSERT INTO Song (name) values (?)", (song.rstrip(),))
+    c.execute("INSERT INTO playlist_manager_song (song_name, album_name, artist_name) values (?, ?, ?)", (song.rstrip(), album.rstrip(), artist.rstrip()))
     songId = c.lastrowid
-    c.execute("INSERT INTO Album2Song VALUES (?, ?)", (albumId, songId))
-    c.execute("INSERT INTO Artist2Song VALUES (?, ?)", (artistId, songId))
+    c.execute("INSERT INTO playlist_manager_album_songs (album_id, song_id) VALUES (?, ?)", (albumId, songId))
+    c.execute("INSERT INTO playlist_manager_artist_songs (artist_id, song_id) VALUES (?, ?)", (artistId, songId))
     song = fh.readline()
 
 ### Close shop
